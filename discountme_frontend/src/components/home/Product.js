a2ea341c386c4987e7eba504abefcd81c36fb509
Md Sakib Hassan <74108908+mdsakibhassan2001@users.noreply.github.com>
2024-12-11 22:19:34 +0600

    product responsive work

--- discountme_frontend/src/components/home/Product.js
+++ discountme_frontend/src/components/home/Product.js
@@ -1,96 +1,134 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 export default function Product() {
   return (
     <div className="py-10 border-t flex flex-col">
-         <h2 className="text-[#232323] text-[40px] font-bold font-['Figtree'] leading-[44px] pb-5">Product</h2>
-         <div className="grid grid-cols-3 gap-2 pb-[23px]">
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-         </div>
-         <div className="grid grid-cols-3 gap-2">
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-         </div>
-         <div className="grid grid-cols-3 gap-2">
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-            <div className="">
-                <div className="relative aspect-[551/287]">
-                    <Image fill src="/assets/product/product1.png" alt="product"/>
-                </div>
-                <div className="flex flex-col mt-2">
-                    <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">Skateboard Shoe</span>
-                    <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">$125</small>
-                </div>
-            </div>
-         </div>
+      <h2 className="text-[#232323] md:text-[40px] text-[30px] font-bold font-['Figtree'] leading-[44px] pb-5">
+        Product
+      </h2>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-2 gap-4 pb-[23px]">
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-2 gap-4 pb-[23px]">
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-2 gap-4 pb-[23px]">
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+        <div className="">
+          <div className="relative aspect-[551/287]">
+            <Image fill src="/assets/product/product1.png" alt="product" />
+          </div>
+          <div className="flex flex-col mt-2">
+            <span className="text-[#232323] text-[21.27px] font-medium font-['Figtree']">
+              Skateboard Shoe
+            </span>
+            <small className="opacity-80 text-[#232323] text-base font-normal font-['Inter'] leading-snug">
+              $125
+            </small>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
