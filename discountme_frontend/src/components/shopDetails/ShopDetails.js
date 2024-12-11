aa24e594e2584d588036ba64f57890239c7556e3
Md Sakib Hassan <74108908+mdsakibhassan2001@users.noreply.github.com>
2024-12-11 23:53:33 +0600

    some work

--- discountme_frontend/src/components/shopDetails/ShopDetails.js
+++ discountme_frontend/src/components/shopDetails/ShopDetails.js
@@ -47,27 +47,27 @@ export default function ShopDetails() {
         />
       </div>
 
-      <h2 className="md:text-[40px] text-[24px] mb-10 py-4 font-[500] border-b border-b-[var(--gray)]">
+      <h2 className="md:text-[40px] text-center md:text-start text-[24px] mb-10 py-4 font-[500] border-b border-b-[var(--gray)]">
         <span className="text-[var(--primary)]">Villagio</span> Restaurant & Bar
       </h2>
       <div className="grid md:grid-cols-2">
         <div className="flex flex-col md:gap-[20px] gap-[10px]">
           <div className="flex items-center mg:gap-[18.11px] gap-[10px]">
-            <MdLocationPin className="text-[var(--primary)] w-[36.23px] h-[36.23px]" />
-            <p className="text-2xl text-[#334a55] flex flex-col font-['Kumbh Sans']">
+            <MdLocationPin className="text-[var(--primary)] md:w-[36.23px] w-[25px] h-[25px] md:h-[36.23px]" />
+            <p className="md:text-2xl text-[18px] text-[#334a55] flex flex-col font-['Kumbh Sans']">
               <span>360 San Lorenzo Avenue, Suite</span>
               <span>1430 Coral Gables, FL 33146-1865 |</span>
             </p>
           </div>
           <div className="flex items-center gap-3">
             <div className="flex gap">
-              <IoMdStar className="text-5xl text-[var(--theme)]" />
-              <IoMdStar className="text-5xl text-[var(--theme)]" />
-              <IoMdStar className="text-5xl text-[var(--theme)]" />
-              <IoMdStar className="text-5xl text-[var(--theme)]" />
-              <IoMdStar className="text-5xl text-[var(--light-gray)]" />
+              <IoMdStar className="md:text-5xl text-3xl text-[var(--theme)]" />
+              <IoMdStar className="md:text-5xl text-3xl text-[var(--theme)]" />
+              <IoMdStar className="md:text-5xl text-3xl text-[var(--theme)]" />
+              <IoMdStar className="md:text-5xl text-3xl text-[var(--theme)]" />
+              <IoMdStar className="md:text-5xl text-3xl text-[var(--light-gray)]" />
             </div>
-            <span className="text-[#1d242d] text-[38.60px] font-bold font-['Urbanist'] tracking-wide">
+            <span className="text-[#1d242d] md:text-[38.60px] text-[24px] font-bold font-['Urbanist'] tracking-wide">
               4.3
             </span>
           </div>
