92a0be55a579b4e83e815315b03d6b61292a77c9
Md Sakib Hassan <74108908+mdsakibhassan2001@users.noreply.github.com>
2024-12-12 01:50:36 +0600

    some work

--- discountme_frontend/src/components/categorieDetails/CategorieDetails.js
+++ discountme_frontend/src/components/categorieDetails/CategorieDetails.js
@@ -21,14 +21,14 @@ export default function CategorieDetails() {
         />
       </div>
       <div className="max-w-[1512px] w-[90%] mx-auto py-8 ">
-        <div className="grid lg:grid-cols-2 gap-[28px]">
-          <div className="lg:grid grid-cols-7 ">
-            <div className=" flex lg:flex-col justify-between  h-auto mr-4 gap-[32px]">
+        <div className="grid grid-cols-2 gap-[28px]">
+          <div className="grid grid-cols-7 ">
+            <div className=" flex flex-col justify-between  h-auto mr-4 gap-[32px]">
               <button className="flex items-center justify-center text-xl text-[var(--gray)] ">
                 <SlArrowUp />
               </button>
-              <div className="flex lg:flex-col gap-[13.8px]">
-                <div className=" border border-[var(--primary)] rounded-[4px] p-[4px] relative  aspect-[93/104] lg:h-full lg:w-full  w-[50px] h-[60px] ">
+              <div className="flex flex-col gap-[13.8px]">
+                <div className=" border border-[var(--primary)] rounded-[4px] p-[4px] relative aspect-[93/104]">
                   <Image
                     alt="login"
                     className="rounded-[4px] h-full w-full"
